9c70a2cdfea239c0c31f5ac11ad31794012005e5
cervantes <cervantes007mail@gmail.com>
2022-06-21 15:51:18 -0400

    feat: add start script and read env port

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const webhooks = [];
 
 const app = express();
 
+app.get("", (req, res) => {
+    res.send("Hello World");
+});
+
 app.post("/hangfire", (req, res) => {
     webhooks.push({url: req.originalUrl, body: req.body})
     res.send("Express on Vercel");
@@ -13,8 +17,8 @@ app.get("/hangfire", (req, res) => {
     res.json(webhooks);
 })
 
-app.listen(5000, () => {
-    console.log("Running on port 5000.");
+app.listen(process.env.PORT, () => {
+    console.log("Running on port 4000.");
 });
 
 // Export the Express API
